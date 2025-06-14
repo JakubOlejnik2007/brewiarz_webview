95c730f4d36f6c81ba2023d9aba756f267572e63
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-06-14 17:11:08 +0200

    Added starting verse and corrected style of intruduction to psalm

--- src/partials/InvitationPsalm.tsx
+++ src/partials/InvitationPsalm.tsx
@@ -11,29 +11,41 @@ const InvitationPsalm = ({ psalm, antiphon }: InvitationPsalmProps) => {
 
     const parts = psalm.psalm.split("\n\n");
 
+    const psalmVerse = psalm.verse as string;
+
     return (
-        <div className="psalm">
-            <h1 className="psalm__title">{psalm.title}</h1>
-            <h2 className="psalm__subtitle">{psalm.subtitle}</h2>
-            <p className="psalm__verse">{psalm.verse}</p>
+        <div>
+            <p>
+                <span className="additional">Przewodniczący - † </span>Panie, otwórz wargi moje. <br />
+                <span className="additional">Wszyscy -</span> A usta moje będą głosić Twoją chwałę.
+            </p>
 
-            <Antiphon antiphon={antiphon} />
-            {
-                parts.map(psalmPart => {
-                    console.log(psalmPart)
-                    return (<><p className="psalm-verse">
-                        {psalmPart.split("\n").map(psalmVerse => <>{psalmVerse}<br /></>)}
+            <div className="psalm">
+                <h1 className="psalm__title">{psalm.title}</h1>
+                <h2 className="psalm__subtitle">{psalm.subtitle}</h2>
+                <p className="psalm__verse">
+                    {psalmVerse.substring(0, psalmVerse.indexOf("("))}
+                    <span className="no-italic">{psalmVerse.substring(psalmVerse.indexOf("("), psalmVerse.length)}</span>
+                </p>
+
+                <Antiphon antiphon={antiphon} />
+                {
+                    parts.map(psalmPart => {
+                        console.log(psalmPart)
+                        return (<><p className="psalm-verse">
+                            {psalmPart.split("\n").map(psalmVerse => <>{psalmVerse}<br /></>)}
 
 
-                    </p>
-                        <Antiphon antiphon={antiphon} />
+                        </p>
+                            <Antiphon antiphon={antiphon} />
 
-                    </>)
+                        </>)
 
 
 
-                })
-            }
+                    })
+                }
+            </div>
         </div>
     )
 }
