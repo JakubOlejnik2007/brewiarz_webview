5a66157a0699d02ad04875400bcb7cebdf6642a6
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-06-14 15:28:58 +0200

    Added input for receiving json file and displaying its content

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import type { FullOffice } from './types/FullOffice.interface';
 function App() {
 
   return (
-    <>
-      
-    </>
+    <main>
+      <input type="file" accept=".json" onChange={handleFileChange} />
+      <ul>{
+        fileContent && fileContent.hours.map((hour, idx) => <li><button>{hour.name}</button></li>)
+      }
+      </ul>
+    </main>
   )
 }
 
