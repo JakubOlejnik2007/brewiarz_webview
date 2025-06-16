95e2a778b74bcc8f7a6fdce6f9630071d21b7e9a
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-06-16 21:39:34 +0200

    Fixed reading sigla when hagiographic reading

--- src/partials/LongReading.tsx
+++ src/partials/LongReading.tsx
@@ -2,12 +2,15 @@ import type { TLongReading } from "../types/LongReading.type"
 import "../sass/LongReading.scss"
 
 const LongReading = ({ number, name, title, sigla, content, ...props }: TLongReading) => {
+
+    const isHagio: boolean = sigla.startsWith("(");
+
     return (
         <div className="lr">
             <p className="lr__number"> {number} Czytanie</p>
-            <div className={`lr__heading ${sigla.startsWith("(") ? "lr__heading_sigla" : ""}`}>
+            <div className={`lr__heading ${isHagio ? "lr__heading_sigla" : ""}`}>
                 <p className="lr__name">{name}</p>
-                <p className="lr__sigla">{sigla}</p>
+                <p className={`lr__sigla ${isHagio ? "lr__sigla--hagio" : ""}`}>{sigla}</p>
             </div>
             <h3 className="lr__title">
                 {title}
