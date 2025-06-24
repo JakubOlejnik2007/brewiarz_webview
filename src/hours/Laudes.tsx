2fb70b334701dc8f42c6b8a953d3a963690f44b9
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-06-24 13:15:30 +0200

    Added laudes component

---
+++ src/hours/Laudes.tsx
@@ -0,0 +1,27 @@
+import Hymn from "../partials/Hymn";
+import Intercessions from "../partials/Intercessions";
+import LV_ending from "../partials/LV_ending";
+import Pater from "../partials/Pater";
+import Prayer from "../partials/Prayer";
+import Psalmodia from "../partials/Psalmodia";
+import Responsory from "../partials/Responsory";
+import ShortReading from "../partials/ShortReading";
+import type { TLaudes } from "../types/Laudes.type";
+
+const Laudes = ({ hymn, psalmodia, reading, intercessions, responsory, prayer }: TLaudes) => {
+    return (
+        <section className="laudes">
+            <h2 className="invitation__heading">Jutrznia</h2>
+            <Hymn hymns={hymn} />
+            <Psalmodia psalmodia={psalmodia} />
+            <ShortReading {...reading} />
+            <Responsory {...responsory} />
+            <Intercessions {...intercessions} />
+            <Pater />
+            <Prayer prayers={prayer} />
+            <LV_ending />
+        </section>
+    )
+}
+
+export default Laudes;
