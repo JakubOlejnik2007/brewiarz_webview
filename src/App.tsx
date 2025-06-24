673354573fc16d88500817f0c36b4a82883adbdf
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-06-24 15:31:53 +0200

    Added responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import './sass/App.scss'
+import './sass/Layout.scss'
 import type { FullOffice } from './types/FullOffice.interface';
 import Invititation from './hours/Invitation';
 import OfficeOfReadings from './hours/OfficeOfReadings';
-import type { TInvitation } from './types/Invitation.type';
-import type { TOfficeOfReadings } from './types/OfficeOfReadings.type';
-import type { TLaudes } from './types/Laudes.type';
 import Laudes from './hours/Laudes';
+import Header from './page/Header';
+import OfficeHeader from './page/OfficeHeader';
 
 function App() {
   const [fileContent, setFileContent] = useState<null | FullOffice>(null);
@@ -36,20 +36,33 @@ function App() {
       .catch(err => console.error("Failed to load default file", err));
   }, []);
 
-  return (
+  return (<>
+    <Header />
+    <nav><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde soluta animi magni! Atque ad explicabo possimus ullam, ex nihil molestiae aut ipsa architecto? Nisi quasi omnis quidem consequuntur. Adipisci, et.
+      Beatae delectus ipsa neque eum ullam culpa tenetur quisquam nam impedit saepe inventore corporis repudiandae, nemo velit veritatis aliquam autem, nisi dolor corrupti accusamus nihil. Aspernatur modi delectus earum molestias.</p></nav>
     <main>
-      <h1>{fileContent?.name}</h1>
-      <input type="file" accept=".json" onChange={handleFileChange} />
-      <ul>{
-        fileContent && fileContent.hours.map((hour, idx) => <li><button onClick={e => setHour(idx)}>{hour.name}</button></li>)
+      {
+        fileContent === null ? <>
+          <input type="file" accept=".json" onChange={handleFileChange} />
+        </> : <>
+          <OfficeHeader officeName={fileContent.name} date={new Date()} />
+          <h1></h1>
+
+          <ul>{
+            fileContent && fileContent.hours.map((hour, idx) => <li><button onClick={e => setHour(idx)}>{hour.name}</button></li>)
+          }
+          </ul>
+          <div className="hour-content">
+            {fileContent && hour === 0 && <Invititation {...(fileContent.hours[0])} />}
+            {fileContent && hour === 1 && <OfficeOfReadings {...(fileContent.hours[1])} />}
+            {fileContent && hour === 2 && <Laudes {...(fileContent.hours[2])} />}
+          </div>
+        </>
       }
-      </ul>
-      <div className="hour-content">
-        {fileContent && hour === 0 && <Invititation {...(fileContent.hours[0])} />}
-        {fileContent && hour === 1 && <OfficeOfReadings {...(fileContent.hours[1])} />}
-        {fileContent && hour === 2 && <Laudes {...(fileContent.hours[2])} />}
-      </div>
+
     </main>
+  </>
+
   )
 }
 
