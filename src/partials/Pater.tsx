568e606a71ad6910d843d0420802bad259349304
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-06-24 10:59:44 +0200

    Added Pater noster prayer

---
+++ src/partials/Pater.tsx
@@ -0,0 +1,16 @@
+const Pater = () => {
+    return (
+        <p>
+            Ojcze  nasz,  któryś jest w  niebie,<br />
+            święć  się  imię  Twoje;<br />
+            przyjdź  królestwo  Twoje;<br />
+            bądź  wola  Twoja jako  w  niebie,  tak  i  na  ziemi.<br />
+            Chleba  naszego  powszedniego  daj  nam  dzisiaj;<br />
+            i odpuść nam nasze winy, jako i my odpuszczamy naszym winowajcom;<br />
+            i  nie  wódź  nas  na  pokuszenie,<br />
+            ale  nas  zbaw  ode  złego.
+        </p>
+    )
+}
+
+export default Pater;
